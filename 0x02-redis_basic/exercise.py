ff6af30ed07a286723d07e87d569a11ac7085ca9
kweku-annan <kwekuannan.dev@gmail.com>
2024-10-24 16:50:34 +0000

    Add solution for task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Creates a Cache class"""
 import redis
-from typing import Union
+from typing import Union, Callable, Optional, Any
 import uuid
 
 
@@ -16,3 +16,20 @@ class Cache:
         rand_key = str(uuid.uuid4())
         key = self._redis.set(rand_key, data)
         return rand_key
+
+    def get(self, key: str, fn: Callable = None):
+        """Takes a key and a Callable and converts data back"""
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """Parametrize get() method with the string type"""
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """Parametrize get() method with the int type"""
+        return self.get(key, lambda x: int(x.decode('utf-8')))
